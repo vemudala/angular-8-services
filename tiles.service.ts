8d00ddee74e603fa05e30b760493841d504dcfdc
vemudala <maheshvemudala@gmail.com>
2019-08-16 14:15:34 +0530

    Update tiles.service.ts

--- tiles.service.ts
+++ tiles.service.ts
@@ -6,46 +6,48 @@ import { ITile } from '../interfaces/itile';
 })
 export class TilesService {
   tiles:Array<ITile>=[{
- 
-    image:'tile.jpg',
-    name:'karjari',
-    model:'2',
-    price:1233,
-    rating:5,
-    status:1,
-    Id:1
-  },{
-   
-    image:'tile4.jpg',
-    name:'sachin',
-    model:'2',
-    price:33,
-    rating:2,
-    status:1,
-    Id:1
-  },{
-   
-    image:'tile2.jpg',
-    name:'kohli',
-    model:'2',
-    price:55,
-    rating:4,
-    status:1,
-    Id:1
-  },{
-   
-    image:'tile3.jpg',
-    name:'dhoni ',
-    model:'2',
-    price:77,
-    rating:3,
-    status:1,
-    Id:1
+    image:'1.jpg',
+      name:'mahesh',
+      model:'developer',
+      price:100,
+      rating:3,
+      status:1
+  },
+  {
+    image:'2.jpg',
+      name:'dhoni',
+      model:'cricketer',
+      price:200,
+      rating:1,
+      status:0
+  },
+  {
+    image:'3.png',
+      name:'kohli',
+      model:'batter',
+      price:500,
+      rating:4,
+      status:1
+  },
+  {
+    image:'4.png',
+      name:'virat',
+      model:'bowl',
+      price:5500,
+      rating:4,
+      status:0
+  },
+  {
+    image:'5.jpg',
+      name:'raj',
+      model:'batter',
+      price:5000,
+      rating:4,
+      status:1
   }]
   
   constructor() { }
   getTiles():Array<ITile>{
     return this.tiles;
   }
-  
 }
