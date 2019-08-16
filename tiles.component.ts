6e7a8d1310fd0a28afe61b1d4e172a73c5eaea6e
vemudala <maheshvemudala@gmail.com>
2019-08-16 14:15:01 +0530

    Update tiles.component.ts

--- tiles.component.ts
+++ tiles.component.ts
@@ -1,40 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
 import {ITile} from '../interfaces/itile';
-import { AlertPromise } from 'selenium-webdriver';
 import { TilesService } from '../services/tiles.service';
+
 @Component({
   selector: 'app-tiles',
   templateUrl: './tiles.component.html',
   styleUrls: ['./tiles.component.css']
 })
 export class TilesComponent implements OnInit {
-  imagepath:string= '/assets/images/';
-  filterOptions : Array<string>=['name','model','price'];
-  selectedoption:string='price';
-  filterText:string='';
-  filteredTiles:Array<ITile>=[];
-tiles:Array<ITile>=[]
+  searcheditems:Array<ITile>=[]
+  imagepath:string='./assets/images/'
+  search:Array<string>=['model','price','name'];
+  selected:string="price";
+  selectedinput:string="";
+tiles:Array<ITile>=[];
   constructor(private tilesService:TilesService) {
-    this.filteredTiles=this.tiles;
+    this.searcheditems=this.tiles;
    }
 
-  ngOnInit():void {
-this.tiles=this.tilesService.getTiles();
-this.filteredTiles=this.tiles;
+  ngOnInit() {
+    this.tiles= this.tilesService.getTiles();
+    this.searcheditems=this.tiles;
   }
-
- filter(){
-   this.filteredTiles=this.myfunc(this.selectedoption,this.filterText,this.tiles);
-   console.log(this.filteredTiles);
- }
-myfunc(dropdn, inp, tilees){
-let filteredTiles = tilees.filter((tile)=>{
-if(dropdn=='price'){
-  return tile.price > +inp;
+  search_filter(){
+    // alert(this.selected);
+    // alert(this.selectedinput);
+    this.searcheditems= this.searchh(this.selected,this.selectedinput,this.tiles);
+console.log(this.searcheditems);
+  }
+  searchh(selected,selectedinput,tilees){
+    let searcheditems= tilees.filter((tilee)=>{
+      if(this.selected=='price'){
+        return tilee.price>+selectedinput
+      }
+    })
+    return searcheditems;
+  }
 }
-
-})
-return filteredTiles;
-}
-} 
